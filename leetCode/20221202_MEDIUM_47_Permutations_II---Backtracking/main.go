aa4cf4c5a89f1f7d3b5c6e1ea29820f5ce38b3f4
koujyungenn <513137767@qq.com>
2022-12-02 12:32:00 +0800

    全排序2-完

---
+++ leetCode/20221202_MEDIUM_47_Permutations_II---Backtracking/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{1, 1, 2}
+	result := permuteUnique(nums)
+	fmt.Println(result)
+}
+
+func permuteUnique(nums []int) [][]int {
+	length := len(nums)
+	used := make([]bool, length)
+	// 记得要指定切片的长度、再指定容量,否则会初始化[0,0,0]，而不是[]，这里踩了一个基础坑
+	path := make([]int, 0, length)
+	result := make([][]int, 0, length)
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	backtracking(nums, used, path, &result)
+	return result
+}
+
+func backtracking(nums []int, used []bool, path []int, result *[][]int) {
+	lenght := len(path)
+	if lenght == len(nums) {
+		// 已经到达叶子节点了，代表已存在一个结果
+		leaf := make([]int, 0, lenght)
+		for _, v := range path {
+			leaf = append(leaf, v)
+		}
+		*result = append(*result, leaf)
+		return
+	}
+	for i, v := range nums {
+		// 纵向不能重复选
+		if used[i] == true {
+			// 这个数字在选择路径上已经使用过了
+			continue
+		}
+		// 横向不能重复选
+		if i > 0 && used[i-1] == true && nums[i] == nums[i-1] {
+			continue
+		}
+		path = append(path, v)
+		used[i] = true
+		backtracking(nums, used, path, result)
+		used[i] = false
+		path = path[0:lenght]
+	}
+}
